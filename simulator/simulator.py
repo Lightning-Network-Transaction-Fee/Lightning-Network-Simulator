5fc7847369d25c3379ede02ccf99dd840c1f359f
Aida Afshar <48694462+AidaAfshar@users.noreply.github.com>
2022-07-04 14:57:45 +0430

    multi channel

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -7,41 +7,38 @@ import time
 import generating_transactions
 
 
-
 #environment has an object of simulator
 class simulator():
   def __init__(self,
-               src,trg,channel_id,
+               src,trgs,channel_ids,
                active_channels, network_dictionary,
                merchants,
-               count,
-               amount,
-               epsilon,
+               transaction_types, # = [(count, amount, epsilon),...]
                node_variables,
                active_providers,
                fixed_transactions = True,
-               with_onchain_rebalancing = False):
+               support_onchain_rebalancing = False):
     
     self.src = src
-    self.trg = trg
-    self.channel_id = channel_id
-    self.count = count
-    self.amount = amount
-
+    self.trgs = trgs
+    self.channel_id = channel_ids
+    self.transaction_types = transaction_types
+    self.number_of_transaction_types = len(transaction_types)
     self.merchants = merchants #list of merchants
-    self.epsilon = epsilon    #ratio of marchant
     self.node_variables = node_variables
     self.active_providers = active_providers
     self.active_channels = active_channels
     self.network_dictionary = network_dictionary
     self.fixed_transactions = fixed_transactions
-    self.with_onchain_rebalancing = with_onchain_rebalancing
-    self.graph = self.generate_graph(amount)
+    self.support_onchain_rebalancing = support_onchain_rebalancing
+
+
+    self.graphs_dict = self.generate_graphs_dict(transaction_types)
 
     if fixed_transactions : 
-      self.transactions = generating_transactions.generate_transactions(amount, count, node_variables, epsilon, active_providers)
+      self.transactions_dict = self.generate_transactions_dict(self, transaction_types, node_variables, active_providers)
     else :
-      self.transactions = None
+      self.transactions_dict = None
  
 
  
@@ -55,7 +52,14 @@ class simulator():
     for (src,trg) in self.active_channels :
       self.network_dictionary[(src,trg)] = self.active_channels[(src,trg)]
       self.network_dictionary[(trg,src)] = self.active_channels[(trg,src)]
-      
+
+
+  def generate_transactions_dict(self, transaction_types, node_variables, active_providers):
+    transactions_dict = dict()
+    for (count, amount, epsilon) in transaction_types :
+        trs = generating_transactions.generate_transactions(amount, count, node_variables, epsilon, active_providers)
+        transactions_dict[amount] = trs
+    return transactions_dict
 
 
   def generate_graph(self, amount):
@@ -70,23 +74,34 @@ class simulator():
 
 
 
+  def generate_graphs_dict(self, transaction_types):
+    graph_dicts = dict()
+    for (count, amount, epsilon) in transaction_types :
+        graph = self.generate_graph(amount)
+        graph_dicts[amount] = graph
+    return graph_dicts
 
-  def update_graph(self, src, trg):
-      if self.is_active_channel(src,trg) :
-        src_trg = self.active_channels[(src,trg)]
-        src_trg_balance = src_trg[0]
-        trg_src = self.active_channels[(trg,src)]
-        trg_src_balance = trg_src[0]
-        
-        if (src_trg_balance <= self.amount) and (self.graph.has_edge(src,trg)):
-          self.graph.remove_edge(src,trg)
-        elif (src_trg_balance > self.amount) and (not self.graph.has_edge(src,trg)): 
-          self.graph.add_edge(src, trg, weight = self.calculate_weight(src_trg, self.amount))
-        
-        if (trg_src_balance <= self.amount) and (self.graph.has_edge(trg,src)):
-          self.graph.remove_edge(trg,src)
-        elif (trg_src_balance > self.amount) and (not self.graph.has_edge(trg,src)): 
-          self.graph.add_edge(trg, src, weight = self.calculate_weight(trg_src, self.amount))
+
+
+  def update_graph(self, src, trg, transaction_amount):
+      graph = self.graphs_dict[transaction_amount]  
+      if self.is_active_channel(src,trg):
+          src_trg = self.active_channels[(src,trg)]
+          src_trg_balance = src_trg[0]
+          trg_src = self.active_channels[(trg,src)]
+          trg_src_balance = trg_src[0]
+          
+          if (src_trg_balance <= transaction_amount) and (graph.has_edge(src,trg)):
+            graph.remove_edge(src,trg)
+          elif (src_trg_balance > transaction_amount) and (not graph.has_edge(src,trg)): 
+            graph.add_edge(src, trg, weight = self.calculate_weight(src_trg, transaction_amount))
+          
+          if (trg_src_balance <= transaction_amount) and (graph.has_edge(trg,src)):
+            graph.remove_edge(trg,src)
+          elif (trg_src_balance > transaction_amount) and (not graph.has_edge(trg,src)): 
+            graph.add_edge(trg, src, weight = self.calculate_weight(trg_src, transaction_amount))
+
+      self.graphs_dict[transaction_amount] = graph
         
     
   
@@ -96,6 +111,7 @@ class simulator():
       if self.is_active_channel(src,trg) :
         self.active_channels[(src,trg)][0] = self.active_channels[(src,trg)][0] - transaction_amount
         self.active_channels[(trg,src)][0] = self.active_channels[(trg,src)][0] + transaction_amount
+        # TODO : adding reward
 
 
 
@@ -106,7 +122,7 @@ class simulator():
         trg = path[i+1]
         if (self.is_active_channel(src, trg)) :
           self.update_active_channels(src,trg,transaction_amount)
-          self.update_graph(src, trg)
+          self.update_graph(src, trg, transaction_amount)
           
           
             
@@ -114,6 +130,7 @@ class simulator():
   def is_active_channel(self, src, trg):
     return ((src,trg) in self.active_channels)
         
+
 
   def onchain_rebalancing(self,onchain_rebalancing_amount,src,trg,channel_id):
     if self.is_active_channel(src,trg) :
@@ -145,53 +162,82 @@ class simulator():
       if self.is_active_channel(src,trg) :
         self.active_channels[(src,trg)][1] = alpha
         self.active_channels[(src,trg)][2] = beta
-        
+
+
+  def set_channels_fees(self,fees) : # fees = [alpha1, alpha2, ..., alphan, beta1, beta2, ..., betan] ~ action
+    n = len(self.trgs)
+    alphas = fees[0:n]
+    betas = fees[n:]
+    src = self.src
+    for i,trg in enumerate(self.trgs):
+        self.network_dictionary[(src,trg)][1] = alphas[i]
+        self.network_dictionary[(src,trg)][2] = betas[i]
+        if self.is_active_channel(src,trg) :
+          self.active_channels[(src,trg)][1] = alphas[i]
+          self.active_channels[(src,trg)][2] = betas[i]
+
 
 
   def run_single_transaction(self,
                              transaction_id,
-                             amount,
+                             transaction_amount,
                              src,trg,
                              graph):
     
     result_bit = 0
+    info = {''}
     try:
       path = nx.shortest_path(graph, source=src, target=trg, weight="weight", method='dijkstra')
       
     except nx.NetworkXNoPath:
-      return None,-1
+      return None,-1,{'no path'}
     val = self.get_path_value(path,graph)
     result_bit = 1
-    return path,result_bit  
+    info = {'successful'}
+    return path, result_bit, info 
 
 
 
+  def preprocess_amount_graph(self,amount,action):
+      graph = self.graphs_dict[amount]
+      src = self.src
+      number_of_channels = len(self.trgs)
+      alphas = action[0:number_of_channels]
+      betas = action[number_of_channels:]
+      for i,trg in enumerate(self.trgs) :
+        if graph.has_edge(src, trg):
+          graph[src][trg]['weight'] = alphas[i]*amount + betas[i]
+      self.graphs_dict[amount] = graph
+      return graph
 
- 
-  def run_simulation(self, count, amount, action):
-      #print("simulating random transactions...")
+
+
+  def run_simulation(self, action):   # action = [alphas, betas] = [alpha1, ..., alphan, beta1, ..., betan]
+    output_transactions_dict = dict()
+    for (count,amount,epsilon) in self.transaction_types:
+        trs = self.run_simulation_for_each_transaction_type(count, amount, epsilon ,action)
+        output_transactions_dict[amount] = trs
+    return output_transactions_dict
+   
+
+
+  def run_simulation_for_each_transaction_type(self, count, amount, epsilon, action):  
+      graph = self.preprocess_amount_graph(amount,action)
       
-
-      #Graph Pre-Processing
-      if self.graph.has_edge(self.src, self.trg):
-        self.graph[self.src][self.trg]['weight'] = action[0]*amount + action[1]
-
-
 
       #Run Transactions
       if self.fixed_transactions : 
-        transactions = self.transactions
+        transactions = self.transactions_dict[amount]
       else :
-        transactions = generating_transactions.generate_transactions(amount, count, self.node_variables, self.epsilon, self.active_providers)
+        transactions = generating_transactions.generate_transactions(amount, count, self.node_variables, epsilon, self.active_providers)
       transactions = transactions.assign(path=None)
       transactions['path'] = transactions['path'].astype('object')
-   
       for index, transaction in transactions.iterrows(): 
-        src,trg = transaction["src"],transaction["trg"]
-        if (not src in self.graph.nodes()) or (not trg in self.graph.nodes()):
-          path,result_bit = [] , -1
+        src,trg = transaction['src'], transaction['trg']
+        if (not src in graph.nodes()) or (not trg in graph.nodes()):
+          path,result_bit, info = [], 1, {'src and/or trg dont exist in the graph'}
         else : 
-          path,result_bit = self.run_single_transaction(transaction["transaction_id"],amount,transaction["src"],transaction["trg"],self.graph) 
+          path,result_bit, info = self.run_single_transaction(transaction['transaction_id'], amount, transaction['src'], transaction['trg'], graph) 
           
         if result_bit == 1 : #successful transaction
             self.update_network_data(path,amount)
@@ -207,10 +253,12 @@ class simulator():
 
  
  
-  """
-  getting the statistics
-  """
 
+
+  #getting the statistics
+
+
+  
   def get_balance(self,src,trg,channel_id):
       self.sync_network_dictionary()
       return self.network_dictionary[(src,trg)][0]
@@ -226,14 +274,23 @@ class simulator():
     return self.network_dictionary
 
 
-  def get_k(self,src,trg,channel_id, transactions):
-    num = 0
-    for index, row in transactions.iterrows():
-        path = row["path"]  
-        for i in range(len(path)-1) :
-          if (path[i]==src) & (path[i+1]==trg) :
-              num += 1
-    return num
+  def get_k_and_tx(self, src, trg, transactions_dict):
+    k = 0
+    tx = 0
+    for amount, transactions in transactions_dict.items():
+      temp_k = 0
+      temp_tx = 0
+      for index, row in transactions.iterrows():
+          path = row["path"]  
+          for i in range(len(path)-1) :
+            if (path[i]==src) & (path[i+1]==trg) :
+                temp_k += 1
+      temp_tx = temp_k*amount
+      k += temp_k
+      tx += temp_tx
+      
+    return k,tx
+
 
 
 
@@ -248,6 +305,8 @@ class simulator():
       alpha_bar += src_trg[1]
       beta_bar += src_trg[2]
     return alpha_bar,beta_bar
+
+
 
 
   def get_excluded_total_fee(self,path, excluded_src, excluded_trg) :
@@ -265,7 +324,7 @@ class simulator():
 
 
 
-  def find_rebalancing_cycle(self,rebalancing_type, src, trg, channel_id, rebalancing_amount):
+  def find_rebalancing_cycle(self, rebalancing_type, src, trg, channel_id, rebalancing_amount):
       rebalancing_graph = self.generate_graph(rebalancing_amount)  
       cheapest_rebalancing_path = []
       
@@ -315,14 +374,30 @@ class simulator():
         rebalancing_fee, rebalancing_type  = self.operate_rebalancing(action[2],src,trg,channel_id,onchain_transaction_fee)
         return k,tx, rebalancing_fee, rebalancing_type
 
+  
+
+
+  def get_simulation_results(self, action, output_transactions_dict):
+        channels_balances = []
+        channels_ks = []
+        channels_txs = []
+        src = self.src
+
+        for i, trg in enumerate(self.trgs):
+          k, tx = self.get_k_and_tx(src, trg, output_transactions_dict)
+          balance = self.active_channels[(src,trg)][0]
+          channels_ks.append(k)
+          channels_txs.append(tx)
+          channels_balances.append(balance)
+
+        return channels_balances, channels_ks, channels_txs
 
 
   def operate_rebalancing(self,gamma,src,trg,channel_id,onchain_transaction_fee):
-    if self.with_onchain_rebalancing==True :
+    if self.support_onchain_rebalancing==True :
         return self.operate_rebalancing_with_onchain(gamma,src,trg,channel_id,onchain_transaction_fee)
     else :
         return self.operate_rebalancing_without_onchain(gamma,src,trg,channel_id)
-
 
 
 
