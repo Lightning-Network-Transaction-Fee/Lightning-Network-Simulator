b036ad14db4c92345cf8dcceab686905ed922e25
Aida Afshar <48694462+AidaAfshar@users.noreply.github.com>
2022-07-04 15:00:10 +0430

    multi channel

--- simulator/generating_transactions.py
+++ simulator/generating_transactions.py
@@ -2,15 +2,13 @@ import numpy as np
 import pandas as pd
 
 
-
 def sample_providers(K,node_variables,active_providers):
       provider_records = node_variables[node_variables["pub_key"].isin(active_providers)]
       nodes = list(provider_records["pub_key"])
       probas = list(provider_records["degree"] / provider_records["degree"].sum())
       return np.random.choice(nodes, size=K, replace=True, p=probas)
-    
-    
-    
+
+
 def generate_transactions(amount_in_satoshi, K, node_variables,epsilon,active_providers,verbose=False):
       nodes = list(node_variables['pub_key'])
       src_selected = np.random.choice(nodes, size=K, replace=True)
@@ -31,4 +29,3 @@ def generate_transactions(amount_in_satoshi, K, node_variables,epsilon,active_pr
           print("Number of loop transactions (removed):", K-len(transactions))
           print("Merchant target ratio:", len(transactions[transactions["target"].isin(active_providers)]) / len(transactions))
       return transactions[["transaction_id","src","trg","amount_SAT"]]
-    
