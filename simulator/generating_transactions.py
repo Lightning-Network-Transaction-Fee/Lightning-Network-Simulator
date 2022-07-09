f7dc09fd177019dcd0e50000873255d3fad93cdf
Aida Afshar <48694462+AidaAfshar@users.noreply.github.com>
2022-07-09 22:24:18 +0430

    exclude src

--- simulator/generating_transactions.py
+++ simulator/generating_transactions.py
@@ -2,19 +2,26 @@ import numpy as np
 import pandas as pd
 
 
-def sample_providers(K,node_variables,active_providers):
+def sample_providers(src, K, node_variables, active_providers, exclude_src=True):
       provider_records = node_variables[node_variables["pub_key"].isin(active_providers)]
-      nodes = list(provider_records["pub_key"])
+      if exclude_src :
+        if src in set(node_variables['pub_key']):
+          nodes = list(set(provider_records["pub_key"]) - set([src]))
+      else :
+        nodes = list(provider_records["pub_key"])
       probas = list(provider_records["degree"] / provider_records["degree"].sum())
       return np.random.choice(nodes, size=K, replace=True, p=probas)
 
-
-def generate_transactions(amount_in_satoshi, K, node_variables,epsilon,active_providers,verbose=False):
-      nodes = list(node_variables['pub_key'])
+def generate_transactions(src, amount_in_satoshi, K, node_variables, epsilon, active_providers, verbose=False, exclude_src=True):
+      if exclude_src :
+        if src in set(node_variables['pub_key']):
+          nodes = list(set(node_variables['pub_key']) - set([src]))
+      else :
+        nodes = list(node_variables['pub_key'])
       src_selected = np.random.choice(nodes, size=K, replace=True)
       if epsilon > 0:
           n_prov = int(epsilon*K)
-          trg_providers = sample_providers(n_prov,node_variables,active_providers)
+          trg_providers = sample_providers(src, n_prov,node_variables,active_providers, exclude_src=True)
           trg_rnd = np.random.choice(nodes, size=K-n_prov, replace=True)
           trg_selected = np.concatenate((trg_providers,trg_rnd))
           np.random.shuffle(trg_selected)
